d0f79c0c8a4d216c98515e00ccac526dca4f5de2
= <=>
2023-01-31 16:30:39 +0600

    max функциясын құрдық

--- test1.py
+++ test1.py
@@ -1,3 +1,5 @@
+# Nurbergen Bakytuly
+
 def square(a, b):
     print(a*b)
 
@@ -6,5 +8,11 @@ def sum(a, b):
     print(a+b)
 
 
+def max(a, b):
+    print(a if a > b else b)
+
+
 square(2, 3)
-sum(a+b)
+
+sum(2, 4)
+max(4, 5)
